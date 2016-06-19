99d1aee95f2f07fe388d5d516b2b9adb30585d3c
Stuart Bowman <bowman.d.stuart@gmail.com>
2016-06-18 21:19:21 -0400

    test application that works

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Stuart to test cppmanifest. This is NOT needed to use cppmanifest!
+//
+#include "include/cppmanifest.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Starting logging" << endl;
+    cout << "My own version: " << cppmanifest::getVersion() << endl;
+    cout << "Created-by: " << cppmanifest::getUserName() << endl;
+
+}
